2c0ed7a0af867839787693772b6188798fd53376
PhuongXD <lanphuong2899@gmail.com>
2019-10-01 17:34:29 +0700

    Ceil&Floor

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Ceil((float) 2.3));
+            Console.WriteLine(Floor((float) -12.3));
+        }
+        public static int Ceil(float x)
+        {
+            int a = (int)x;
+            if(x > 0)
+                if(x > a)
+                a = a + 1;
+            return a;
+        }
+        public static int Floor(float x)
+        {
+            int a = (int)x;
+            if (x < 0 && x!=a)
+            {
+               a = a - 1 ;
+            }
+            else
+            {
+                a = a;
+            }
+
+            return a;
+        }
+    }
+}
